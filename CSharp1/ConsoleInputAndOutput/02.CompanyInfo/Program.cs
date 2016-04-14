26ac15c77cb92893b4eab91a77fcce8a2ddf13f8
Alexander Staikov <Alexander Staikov>
2016-04-14 19:50:20 +0300

    added 2, 3

---
+++ CSharp1/ConsoleInputAndOutput/02.CompanyInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.CompanyInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string company = Console.ReadLine();
+            string address = Console.ReadLine();
+            string companyTel = Console.ReadLine();
+            string fax = Console.ReadLine();
+            string site = Console.ReadLine();
+            string fName = Console.ReadLine();
+            string lname = Console.ReadLine();
+            string age = Console.ReadLine();
+            string personTel = Console.ReadLine();
+
+            if (fax == "")
+            {
+                fax = "(no fax)";
+            }
+            Console.WriteLine(company);
+            Console.WriteLine("Address: {0}",address);
+            Console.WriteLine("Tel. {0}",companyTel);
+            Console.WriteLine("Fax: {0}", fax);
+            Console.WriteLine("Web site: {0}", site);
+            Console.WriteLine("Manager: {0} {1} (age: {2}, tel. {3})",fName, lname, age, personTel);
+             
+        }
+    }
+}
---
+++ CSharp1/ConsoleInputAndOutput/03.Circle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = int.Parse(Console.ReadLine());
+            double perimeter = 2 * radius * Math.PI;
+            double area = radius * radius * Math.PI;
+
+            Console.WriteLine("{0:F2} {1:F2}",perimeter ,area);
+        }
+    }
+}
