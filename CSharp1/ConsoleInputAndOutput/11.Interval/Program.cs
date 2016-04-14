0feed6dd8e7bd30d0ef2e86a0d88721aca497414
Alexander Staikov <Alexander Staikov>
2016-04-14 21:58:04 +0300

    testing

--- CSharp1/ConsoleInputAndOutput/11.Interval/Program.cs
+++ CSharp1/ConsoleInputAndOutput/11.Interval/Program.cs
@@ -10,6 +10,18 @@ namespace _11.Interval
     {
         static void Main(string[] args)
         {
+            //te
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = a + 1; i < b; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
         }
     }
 }
