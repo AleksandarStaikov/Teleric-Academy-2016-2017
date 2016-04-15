e4e862bb37b0650cf5a9a6c56258bf1831ecf6d8
Alexander Staikov <Alexander Staikov>
2016-04-15 09:50:54 +0300

    conditianal statemants

---
+++ CSharp1/ConditionalStatemnts/03.PlayCard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.PlayCard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int number = 0;
+            if (int.TryParse(input, out number))
+            {
+                if (number > 1 && number < 11)
+                {
+                    Console.WriteLine("yes {0}", number);
+                } 
+            }
+        }
+    }
+}
--- CSharp1/ConsoleInputAndOutput/09.SumOFnNumbers/Program.cs
+++ CSharp1/ConsoleInputAndOutput/09.SumOFnNumbers/Program.cs
@@ -10,6 +10,12 @@ namespace _09.SumOFnNumbers
     {
         static void Main(string[] args)
         {
+            int sum = 0, n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
         }
     }
 }
