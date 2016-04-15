e71f752c9ee8e22a974d0ddb6e6ac8b97d2509f3
Alexander Staikov <Alexander Staikov>
2016-04-15 11:11:26 +0300

    Conditional Statemnts homework finished

---
+++ CSharp1/ConditionalStatemnts/04.MultiplicationSign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.MultiplicationSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double result = double.Parse(Console.ReadLine());
+
+            if ((a + b) == result)
+            {
+                Console.WriteLine("+");
+            }
+            else if ((a - b) == result)
+            {
+                Console.WriteLine("-");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+
+    }
+}
---
+++ CSharp1/ConditionalStatemnts/10.IntDoubleString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.IntDoubleString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string varType = Console.ReadLine();
+            if (varType == "string")
+            {
+                string word = Console.ReadLine();
+                Console.WriteLine("{0}*",word);
+            }
+        }
+    }
+}
