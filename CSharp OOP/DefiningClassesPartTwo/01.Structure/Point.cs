0598efaa6fc517f7a84643c87f70e2ef3691825f
Alexander Staikov <Alexander Staikov>
2016-06-11 22:56:47 +0300

    started defining classes part two homework

---
+++ CSharp OOP/DefiningClassesPartTwo/01.Structure/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Structure
+{
+    public struct Point
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+        private int z { get; set; }
+
+        public Point(int X, int Y, int Z)
+            : this()
+        {
+            this.x = X;
+            this.y = Y;
+            this.z = Z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}-{1}-{2}", x, y, z);
+        }
+    }
+}
---
+++ CSharp OOP/DefiningClassesPartTwo/01.Structure/StartingPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Structure
+{
+    class StartingPoint
+    {
+        static void Main(string[] args)
+        {
+            var myPoint = new Point(2,4,7);
+
+            Console.WriteLine(myPoint.ToString());
+        }
+    }
+}
